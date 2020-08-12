4dd426b3aea63458c78faf26403896a1e53c42bc
likatare <55482170+likatare@users.noreply.github.com>
2020-08-12 14:42:21 +0200

    Second Commit
    Added hash + salt to password

--- ITSakApp/Program.cs
+++ ITSakApp/Program.cs
@@ -5,16 +5,19 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace ITSakApp
 {
     class Program
     {
+        private static readonly Random random = new Random();
+
         static void Main(string[] args)
         {
             Console.WriteLine("This is itsäk test app!");
 
-            
+
 
             while (true)
             {
@@ -42,7 +45,8 @@ namespace ITSakApp
                     case 6:
                         ReadFile();
                         break;
-                    default:exit = true;
+                    default:
+                        exit = true;
                         break;
                 }
 
@@ -53,40 +57,7 @@ namespace ITSakApp
             }
         }
 
-        private static void ReadFile()
-        {
-           // string text = System.IO.File.ReadAllText(@"C:\Users\toffa\Documents\It och säkerhet\Kalles kaviar.txt");
-           // System.Console.WriteLine("Contents of Kalles kaviar.txt = {0}", text);
 
-            const string dir = @"C:\Users\toffa\Documents\It och säkerhet";  // your folder here
-            var files = Directory.GetFiles(dir);
-            for (var i = 0; i < files.Length; i++)
-            {
-                Console.WriteLine($"{i}) {Path.GetFileName(files[i])}");
-            }
-
-            while (true)
-            {
-                Console.WriteLine("Enter the number of the file you would like to see or -1 to exit");
-                var input = Console.ReadLine();
-
-                if (int.TryParse(input, out var index) && index > -2 && index < files.Length)
-                {
-                    if (index == -1)
-                        return;
-
-                    string[] chosenFile = System.IO.File.ReadAllLines(files[index]);
-                    foreach (string file in chosenFile)
-                    {
-                        Console.WriteLine(file);
-                    }
-                   
-                }
-                else
-                    Console.WriteLine("Bad input. Repeat please.");
-            }
-            
-        }
 
         private static void CreateUser()
         {
@@ -99,10 +70,12 @@ namespace ITSakApp
             Console.Write("Enter password: ");
             string password = Console.ReadLine();
 
+            string passwordHash = HashPassword(password);
+
             Console.Write("Enter description: ");
             string description = Console.ReadLine();
 
-            UserRepository.CreateUser(username,password,description);
+            UserRepository.CreateUser(username, passwordHash, description);
         }
 
         private static void DeleteUser()
@@ -176,6 +149,72 @@ namespace ITSakApp
             UserRepository.CreateNote(userToNote.Id, userToNote);
         }
 
+        private static void ReadFile()
+        {
+            // string text = System.IO.File.ReadAllText(@"C:\Users\toffa\Documents\It och säkerhet\Kalles kaviar.txt");
+            // System.Console.WriteLine("Contents of Kalles kaviar.txt = {0}", text);
+
+            const string dir = @"C:\Users\toffa\Documents\It och säkerhet";  // your folder here
+            var files = Directory.GetFiles(dir);
+            for (var i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine($"{i}) {Path.GetFileName(files[i])}");
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter the number of the file you would like to see or -1 to exit");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out var index) && index > -2 && index < files.Length)
+                {
+                    if (index == -1)
+                        return;
+
+                    string[] chosenFile = System.IO.File.ReadAllLines(files[index]);
+                    foreach (string file in chosenFile)
+                    {
+                        Console.WriteLine(file);
+                    }
+
+                }
+                else
+                    Console.WriteLine("Bad input. Repeat please.");
+            }
+
+        }
+
+        static string HashPassword(string password)
+        {
+            string salt = RandomString(25);
+            string saltedPassword = $"{password}:{salt}";
+
+            string md5 = CreateMd5(saltedPassword);
+
+            return $"{md5}{salt}";
+        }
+
+        private static string CreateMd5(string input)
+        {
+            MD5 mD5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = mD5.ComputeHash(inputBytes);
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars,length)
+                .Select(s=>s[random.Next(s.Length)]).ToArray());
+        }
+
         private static User SelectUser()
         {
             List<User> users = UserRepository.GetUsers();
@@ -206,7 +245,7 @@ namespace ITSakApp
             Console.Write("Input selection: ");
 
             string input = Console.ReadLine();
-             int.TryParse(input,out int selectedOption);
+            int.TryParse(input, out int selectedOption);
             return selectedOption;
 
         }
